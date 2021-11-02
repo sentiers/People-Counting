aea15f5f37660cdb5d2b6faf24b3b8a6f0ede6cb
Hyunji Cho <sentiersg@gmail.com>
2021-11-03 02:09:31 +0900

    Add intersect and ccw function

--- track.py
+++ track.py
@@ -27,6 +27,14 @@ import cv2
 import torch
 import torch.backends.cudnn as cudnn
 
+##############################################################################
+# Return true if line segments AB and CD intersect
+def intersect(A, B, C, D):
+    return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
+# Return true if ABC is counterclockwise
+def ccw(A, B, C):
+    return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+##############################################################################
 
 def detect(opt):
     out, source, yolo_weights, deep_sort_weights, show_vid, save_vid, save_txt, imgsz, evaluate, half = \
