8e609910d398575f9dbf04c7026c9cb714e42c84
Hyunji Cho <sentiersg@gmail.com>
2021-12-13 00:41:13 +0900

    Update root.js and index.ejs

--- app/routes/root.js
+++ app/routes/root.js
@@ -7,7 +7,6 @@ router.get('/', function (req, res, next) {
     Record.find().limit(1).sort({ "time": -1 })
         .then((data) => {
             res.render('index', data[0]);
-            console.log(data[0]);
         })
 });
 
